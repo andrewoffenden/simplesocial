97b0cdcfb4b546da115476e956854d939729127a
Douglas Post <dpost@isci.rjs>
2020-10-13 10:16:51 -0600

    Started Simple Social2 models and admin

--- simplesocial2/admin.py
+++ simplesocial2/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+#from django.contrib.auth import get_user_model
+#from django.contrib.auth.admin import UserAdmin
+#from .models import UserProfile, UserPost, UserPostComment, SitePost
+from . import models as ssoc
 
 # Register your models here.
-#admin.site.register()
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+admin.site.register(ssoc.UserProfile, UserProfileAdmin)
+
+class UserPostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ssoc.UserPost, UserPostAdmin)
+
+class UserPostCommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ssoc.UserPostComment, UserPostCommentAdmin)
+
+class SitePostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ssoc.SitePost, SitePostAdmin)
+
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'value']
+
+admin.site.register(ssoc.SiteSetting, SiteSettingAdmin)
--- simplesocial2/models.py
+++ simplesocial2/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from accounts.models import CustomUser
 
 # Create your models here.
+User = get_user_model()
+
+class UserProfile(models.Model):
+    username = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    friends = models.ManyToManyField('self',blank=True)
+    
+    def __str__(self):
+        return str(self.username)
+
+class UserPost(models.Model):
+    username = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    post = models.TextField(max_length=1024)
+    postpic = models.FileField(null=True,blank=True)
+    postdate = models.DateTimeField(auto_now_add=True)
+    postlike = models.ManyToManyField(User, related_name='postlikes')
+
+class UserPostComment(models.Model):
+    postid = models.ForeignKey(
+        UserPost,
+        on_delete=models.CASCADE,
+    )
+    username = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    comment = models.TextField(max_length=1024)
+    commentpic = models.FileField(null=True,blank=True)
+    commentdate = models.DateTimeField(auto_now_add=True)
+
+class SitePost(models.Model):
+    username = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    post = models.TextField(max_length=1024)
+    postpic = models.FileField(null=True,blank=True)
+    postdate = models.DateTimeField(auto_now_add=True)
+
+class SiteSetting(models.Model):
+    # This is a name value pair of various settings
+    # for the site.
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
